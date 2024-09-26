3c9110c89956429d0df948ee9ca54dfdf55d4f2d
crystal <crystalokd@gmail.com>
2024-09-26 09:42:07 +0100

    ....

--- browser.py
+++ browser.py
@@ -1,4 +1,29 @@
 import socket
+import tkinter
+
+WIDTH, HEIGHT = 800, 600
+SCROLL_STEP = 100
+
+def lex(body):
+    text = ""
+    in_tag = False
+    for c in body:
+        if c == "<":
+            in_tag = True
+        elif c == ">":
+            in_tag = False
+        elif not in_tag:
+            text += c
+    return text
+
+def layout(text):
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        # ...
+    return display_list
+
 
 class URL:
     def __init__(self, url):
@@ -61,22 +86,42 @@ class URL:
         return body
 
 
-def show(body):
-    in_tag = False
-    for c in body:
-        if c == "<":
-            in_tag = True
-        elif c == ">":
-            in_tag = False
-        elif not in_tag:
-            print(c, end="")
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(
+            self.window, 
+            width=WIDTH,
+            height=HEIGHT
+        )
+        self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrolldown)
+
+    def load(self, url):
+        body = url.request()
+        text = lex(body)
+        self.display_list = layout(text)
+        self.draw()
+
+    def draw(self):
+        self.canvas.delete("all")
+        for x, y, c in self.display_list:
+            self.canvas.create_text(x, y, text=c)
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
 
 
-def load(url):
-    body = url.request()
-    show(body)
+
+
+
+
+
 
 
 if __name__ == "__main__":
     import sys
-    load(URL(sys.argv[1]))
+    Browser().load(URL(sys.argv[1]))
+    tkinter.mainloop()
